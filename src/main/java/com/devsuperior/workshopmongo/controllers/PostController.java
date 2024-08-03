b8c2514105827218438035170056b46e16fa4a6e
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-08-03 19:52:00 -0300

    add findByTitle Post

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,15 +1,16 @@
 package com.devsuperior.workshopmongo.controllers;
 
+import com.devsuperior.workshopmongo.controllers.util.URL;
 import com.devsuperior.workshopmongo.dto.PostDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.workshopmongo.services.PostService;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -24,13 +25,13 @@ public class PostController {
 				.map(postDTO -> ResponseEntity.ok().body(postDTO));
 	}
 
-	/*
 	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+	public Flux<PostDTO> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
 		text = URL.decodeParam(text);
-		List<PostDTO> list = service.findByTitle(text);
-		return ResponseEntity.ok(list);
+		return service.findByTitle(text);
 	}
+
+	/*
 	
 	@GetMapping(value = "/fullsearch")
 	public ResponseEntity<List<PostDTO>> fullSearch(
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -20,12 +21,12 @@ public class PostService {
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
 	}
 
-	/*
-	public List<PostDTO> findByTitle(String text) {
-		List<PostDTO> result = repository.searchTitle(text).stream().map(x -> new PostDTO(x)).toList();
-		return result;
+	public Flux<PostDTO> findByTitle(String text) {
+		return repository.searchTitle(text)
+				.map(postFound -> new PostDTO(postFound));
 	}
-	
+
+	/*
 	public List<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
 		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
 		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
